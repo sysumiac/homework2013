b72aea98d63a84d5434be3adf48e8500017ad8a7
khhhshhh <iammfw@163.com>
2013-11-30 00:06:34 +0800

    add ly file

--- lecture3-javascript-basic/赖渊/my-gallery/js/comment.js
+++ lecture3-javascript-basic/赖渊/my-gallery/js/comment.js
@@ -1,26 +1,23 @@
+document.getElementById("comment-submit").addEventListener('click', list);
 var count = 1;
 function list() {
+
 	var text = document.getElementById("comment_text");
-	var comment_list = document.getElementById("comments_list");
+	var commentList = document.getElementById("comments_list");
 	var li = document.createElement("li");
 	var p = document.createElement("p");
-	var input_text = document.createTextNode(text.value);
-	if (input_text.length < 4) {
-	alert("输入的内容少于4个字符，不允许发布。");
+	var inputText = document.createTextNode(text.value);
+
+	if (inputText.length < 4) {
+		alert("输入的内容少于4个字符，不允许发布。");
 	} else {
-		p.appendChild(input_text);
-		p.style.color = "#D7EBFC";
-		p.style.fontSize = "20px";
-		p.style.lineHeight = "1.5em"
+		p.appendChild(inputText);
 		li.appendChild(p);
-		li.style.borderTop = "1px solid #EEE";
-	    li.style.borderBottom = "1px solid #EEE";
-		li.style.listStyle = "none";
-		li.style.margin = "30px 0";
-		li.style.padding = "8px 0";
-		comment_list.appendChild(li);
+		li.className = 'comment-item';
+		commentList.appendChild(li);
 		text.value = "";
-		count += 1;
+		count++;
 		counter.innerHTML = count;
-	  }
+	}
+	
 }
