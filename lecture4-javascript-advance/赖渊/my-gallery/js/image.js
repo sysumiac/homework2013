ddb09e6277bee620cc8af734fc5252cbe1745437
khhhshhh <iammfw@163.com>
2013-12-10 11:28:13 +0800

    add tween-scroller

--- lecture4-javascript-advance/赖渊/my-gallery/js/image.js
+++ lecture4-javascript-advance/赖渊/my-gallery/js/image.js
@@ -1,61 +1,47 @@
-var pic1 = document.getElementById('gallery-slide1');
-var pic2 = document.getElementById('gallery-slide2');
-var pic3 = document.getElementById('gallery-slide3');
-var ctrl1 = document.getElementById('control-circle1');
-var ctrl2 = document.getElementById('control-circle2');
-var ctrl3 = document.getElementById('control-circle3');
+var pics = [
+  document.getElementById('gallery-slide1'),
+  document.getElementById('gallery-slide2'),
+  document.getElementById('gallery-slide3')
+]
 
-var fadeIn = 0;
-var fadeOut = 1;
-var pic = pic3;
+var ctrls = [
+  document.getElementById('control-circle1'),
+  document.getElementById('control-circle2'),
+  document.getElementById('control-circle3')
+]
 
-pic1.className = "opacity-none";
-pic2.className = "opacity-none";
+var currentPicIndex = 0
+var fadeInTimer = null
+var fadeOutTimer = null
 
-ctrl1.addEventListener('click', function() {
-    showPic(pic1);
-});
+var ANIMATION_FRAME = 1000 / 60
+var ANIMATION_TIME = 3000
 
-ctrl2.addEventListener('click', function() {
-    showPic(pic2);
-});
-
-ctrl3.addEventListener('click', function() {
-    showPic(pic3);
-});
-
-function showPic(picx) {
-
-   if (pic != picx) {
-        if (fadeIn < 1) {
-            fadeIn += 0.01;
-            fadeOut -= 0.01;
-            picx.style.opacity = fadeIn;
-            pic.style.opacity = fadeOut;
-            setTimeout(function() {
-                showPic(picx);
-            }, 3);
-        } else {
-            fadeIn = 0;
-            fadeOut = 1;
-            pic = picx;
-        }
-    }
-
+function addActionToController() {
+  for (var i = 0, len = ctrls.length; i < len; i++) {
+    ctrls[i].addEventListener('click', (function(index) {
+      return function() {
+        fadeByIndex(index)
+        fadeByIndex(currentPicIndex, true)
+      }
+    })(i), false)
+  }
 }
- 
-setInterval(carousel, 7500);
 
-carousel();
+function fadeByIndex(index, isFadeOut) {
+  var pic = pics[index]
+  var opacity = isFadeOut ? 0: 1
+  var sign = isFadeOut ? -1: 1
+  var timer = isFadeOut ? fadeOutTimer: fadeInTimer
 
-function carousel() {
-
-    showPic(pic3);
-    setTimeout(function() {
-        showPic(pic2);
-    }, 2500);
-    setTimeout(function() {
-        showPic(pic1);
-    }, 5000);
-
+  clearTimeout(fadeInTimer)
+  pic.style.opacity = opacity
+  
+  timer = setTimeout(function() {
+    opacity += sign * 1 / (ANIMATION_TIME / ANIMATION_FRAME)
+    pic.style.opacity = opacity
+    timer = setTimeout(arguments.callee, ANIMATION_FRAME)
+  }, ANIMATION_FRAME)
 }
+
+addActionToController()
